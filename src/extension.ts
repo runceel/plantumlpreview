21bde62a0008702c3c5a51a92ebc3b9bca8dc72d
Kazuki <oota.kazuki@jp.fujitsu.com>
2016-09-06 15:59:40 +0900

    del temp env val and refactor.

--- src/extension.ts
+++ src/extension.ts
@@ -7,27 +7,75 @@ import * as Q from 'q';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-    if (!process.env['PLANTUML_HOME'] || !process.env['JAVA_HOME'] || !process.env['TEMP']) {
+    if (!process.env['PLANTUML_HOME'] || !process.env['JAVA_HOME']) {
         if (!process.env['PLANTUML_HOME']) { vscode.window.showErrorMessage('Set enviroment variable. PLANTUML_HOME.'); } 
         if (!process.env['JAVA_HOME']) { vscode.window.showErrorMessage('Set enviroment variable. JAVA_HOME.'); } 
-        if (!process.env['TEMP']) { vscode.window.showErrorMessage('Set enviroment variable. TEMP.'); } 
         return;
     }
 
     // 定数
     const isDebug = !!process.env['OKAZUKIUML_DEBUG'];
-    const plantumlCommand = path.join(process.env['PLANTUML_HOME'], 'plantuml.jar');
-    const javaCommand = path.join(process.env['JAVA_HOME'], 'bin', 'java');
-    const outputPath = path.join(process.env['TEMP'], 'okazukiplantuml');
-    try {
-        fs.accessSync(outputPath);
-    } catch (e) {
-        fs.mkdirSync(outputPath);
+
+    class FsUtils {
+        public static mkdirExSync(dirPath: string): void {
+            try {
+                fs.mkdirSync(dirPath);
+            } catch (e) {
+                FsUtils.mkdirExSync(path.dirname(dirPath));
+                FsUtils.mkdirExSync(dirPath);
+            }
+        }
     }
-    
+
+    class PlantUML {
+        public static fromTextEditor(editor: vscode.TextEditor): PlantUML {
+            return new PlantUML(
+                path.dirname(editor.document.uri.fsPath),
+                editor.document.getText().trim()
+            );
+        }
+
+        private static plantUmlCommand = path.join(process.env['PLANTUML_HOME'], 'plantuml.jar');
+        private static javaCommand = path.join(process.env['JAVA_HOME'], 'bin', 'java');
+
+        constructor(private workDir: string, 
+            private plantUmlText: string,
+            private args: string[] = ['-p', '-tsvg', '-charset', 'utf-8']) {
+        }
+
+        public execute(): Q.Promise<string> {
+            let params = ['-Duser.dir=' + this.workDir, '-Djava.awt.headless=true', '-jar', PlantUML.plantUmlCommand];
+            params.push(...this.args);
+            let process = child_process.spawn(PlantUML.javaCommand, params);
+            process.stdin.write(this.plantUmlText);
+            process.stdin.end();
+            return Q.Promise<string>((resolve, reject, notify) => {
+                var output = '';
+                process.stdout.on('data', x => {
+                    output += x;
+                });
+                process.stdout.on('close', () => {
+                    resolve(output);
+                });
+
+                let stderror = '';
+                process.stderr.on('data', x => {
+                     stderror += x; 
+                });
+                process.stderr.on('close', () => {
+                    if (isDebug && !!stderror) {
+                        vscode.window.showErrorMessage(stderror);
+                    }
+                });
+            });
+        }
+    }
+
     // ContentProvider
     class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
         private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
+
+        private svg: string = '<svg></svg>';
 
         public provideTextDocumentContent(uri: vscode.Uri): string {
             return this.createPlantumlSnippet();
@@ -37,7 +85,8 @@ export function activate(context: vscode.ExtensionContext) {
             return this._onDidChange.event;
         }
 
-        public update(uri: vscode.Uri) {
+        public update(uri: vscode.Uri, svg: string) {
+            this.svg = svg;
             this._onDidChange.fire(uri);
         }
 
@@ -51,9 +100,7 @@ export function activate(context: vscode.ExtensionContext) {
 
         private extractSnippet(): string {
             let editor = vscode.window.activeTextEditor;
-            var r = `<body>
-                <img src='file://` + path.join(outputPath, path.basename(editor.document.uri.fsPath, path.extname(editor.document.uri.fsPath))) + `.png?dummy=` + new Date().getTime() + `' />
-            </body>`;
+            var r = '<body>' + this.svg + '</body>';
             return r;
         }
 
@@ -64,34 +111,6 @@ export function activate(context: vscode.ExtensionContext) {
         }
     }
 
-    // editorの内容をプレビューする
-    function executePreview(editor: vscode.TextEditor): Q.Promise<{}> {
-        let q = Q.defer();
-        let tempFilePath = path.join(outputPath,
-            path.basename(vscode.workspace.rootPath),
-            editor.document.uri.fsPath.substr(vscode.workspace.rootPath.length));
-        try {
-            fs.accessSync(path.dirname(tempFilePath));
-        } catch (e) {
-            mkdirExSync(path.dirname(tempFilePath));
-        }
-
-        fs.writeFile(tempFilePath, editor.document.getText(), (err) => {
-            if (isDebug) {
-                if (err) {
-                    vscode.window.showErrorMessage(err.message);
-                }
-            }
-            child_process.exec(buildPlantUMLCommand(javaCommand, plantumlCommand, outputPath, tempFilePath), (error, stdout, stderr) => {
-                showDebugError(isDebug, error, stderr);
-                provider.update(previewUri);
-                q.resolve();
-            });
-        });
-
-        return q.promise;
-    }
-
     let provider = new TextDocumentContentProvider();
     let registration = vscode.workspace.registerTextDocumentContentProvider('plantuml-preview', provider);
 
@@ -100,34 +119,42 @@ export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('extension.previewPlantUML', () => {
         let editor = vscode.window.activeTextEditor;
         var d = Q.defer();
-        executePreview(editor).then(_ =>  {
-            vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'PlantUML Preview')
-                .then((success) => {
-                    provider.update(previewUri);
-                    d.resolve(); 
-                }, (reason) => { 
-                    vscode.window.showErrorMessage(reason); 
-                    d.resolve();
-                });            
-        });
+        PlantUML.fromTextEditor(editor)
+            .execute()
+            .then(svg => {
+                vscode.commands.executeCommand('vscode.previewHtml', previewUri, vscode.ViewColumn.Two, 'PlantUML Preview')
+                    .then((success) => {
+                        provider.update(previewUri, svg);
+                        d.resolve(); 
+                    }, (reason) => { 
+                        vscode.window.showErrorMessage(reason); 
+                        d.resolve();
+                    });            
+            })
         return d.promise;
     });
 
     let exportDisposable = vscode.commands.registerCommand('extension.exportPlantUML', () => {
         let editor = vscode.window.activeTextEditor;
         let exportPath = path.dirname(editor.document.uri.fsPath);
-        child_process.exec(buildPlantUMLCommand(javaCommand, plantumlCommand, exportPath, editor.document.uri.fsPath), (error, stdout, stderr) => {});
     });
 
     let saveTextDocumentDisposable = vscode.workspace.onDidSaveTextDocument((e: vscode.TextDocument) => {
         if (e === vscode.window.activeTextEditor.document) {
-            let editor = vscode.window.activeTextEditor;
-            executePreview(editor);
+            PlantUML.fromTextEditor(vscode.window.activeTextEditor)
+                .execute()
+                .then(svg => {
+                    provider.update(previewUri, svg);
+                });
         }
     });
 
     let activeEditorChangedDisposable = vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor) => {
-        executePreview(editor);
+            PlantUML.fromTextEditor(editor)
+                .execute()
+                .then(svg => {
+                    provider.update(previewUri, svg);
+                });
     });
 
     // 変更時のプレビュー更新
@@ -137,17 +164,16 @@ export function activate(context: vscode.ExtensionContext) {
         changedTimestamp = new Date().getTime();
         setTimeout(() => {
             if (new Date().getTime() - changedTimestamp >= 400) {
-                executePreview(vscode.window.activeTextEditor);
+                PlantUML.fromTextEditor(vscode.window.activeTextEditor)
+                    .execute()
+                    .then(svg => {
+                        provider.update(previewUri, svg);
+                    });
             }
         }, 500);
     });
 
     context.subscriptions.push(disposable, exportDisposable, saveTextDocumentDisposable, activeEditorChangedDisposable, selectionChangedDisposable);        
-}
-
-function buildPlantUMLCommand(javaCommand: string, plantumlCommand: string, outputPath: string, targetPath: string) {
-    return '"' + javaCommand + '" -Djava.awt.headless=true -jar "' + plantumlCommand + '" "' +
-        targetPath + '" -o "' + outputPath + '" -charset utf-8';
 }
 
 function showDebugError(isDebug: boolean, error: Error, stderr: string) {
@@ -161,23 +187,15 @@ function showDebugError(isDebug: boolean, error: Error, stderr: string) {
     }        
 }
 
-// mkdir recurcive
-function mkdirExSync(dirPath: string) {
-    try {
-        fs.mkdirSync(dirPath);
-    } catch (e) {
-        mkdirExSync(path.dirname(dirPath));
-        mkdirExSync(dirPath);
-    }
-}
-
 // this method is called when your extension is deactivated
 export function deactivate() {
     let isDebug = !!process.env['OKAZUKIUML_DEBUG'];
     let outputPath = path.join(process.env['TEMP'], 'okazukiplantuml');
     fs.rmdir(outputPath, err => {
         if (isDebug) {
-            vscode.window.showErrorMessage(err.message);
+            if (err) {
+                vscode.window.showErrorMessage(err.message);
+            }
         }
     });
         
